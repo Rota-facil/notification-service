9c6a9d3601b5155d59c946fd880842be534303da
ferreira-gn <geanoficial14@gmail.com>
2026-05-05 16:21:02 -0300

    refactor : rename queues and consumers of rabbit-mq

--- src/main/java/com/rota/facil/notification_service/messaging/consumers/RabbitTransportEventConsumer.java
+++ src/main/java/com/rota/facil/notification_service/messaging/consumers/RabbitTransportEventConsumer.java
@@ -8,10 +8,13 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class RabbitTransportEventConsumer {
-    // injeta um EmailService para fazer as operacoes de lógica de negócio para envio de email
 
-    @RabbitListener(queues = "${rabbitmq.notification.route.cancelled.queue}")
-    public void handlerRouteCancelled(TransportRouteCancelledEventReceive transportRouteCancelledEventReceive) {
-        // chama service para enviar os email de cancelamento de viagem
-    }
+  // injeta um EmailService para fazer as operacoes de lógica de negócio para envio de email
+
+  @RabbitListener(queues = "${rabbitmq.notification.trip.cancelled.queue}")
+  public void handlerRouteCancelled(
+    TransportRouteCancelledEventReceive transportRouteCancelledEventReceive
+  ) {
+    // chama service para enviar os email de cancelamento de viagem
+  }
 }
