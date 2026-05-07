98dab89bf818fe5e8a001bd76d90a7e8067e21cc
ferreira-gn <geanoficial14@gmail.com>
2026-05-07 11:08:13 -0300

    fix : Correct date formatting in the Thymeleaf template.

--- src/main/java/com/rota/facil/notification_service/messaging/dto/receive/TransportRouteCancelledEventReceive.java
+++ src/main/java/com/rota/facil/notification_service/messaging/dto/receive/TransportRouteCancelledEventReceive.java
@@ -1,17 +1,18 @@
 package com.rota.facil.notification_service.messaging.dto.receive;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public record TransportRouteCancelledEventReceive(
   RouteDto route,
-  List<SubscriberDto> subscribes
+  List<SubscriberDto> subscribers
 ) {
   public record RouteDto(
     @JsonProperty("reasson_of_cancelled") String reassonOfCancelled,
     @JsonProperty("prefecture_name") String prefectureName,
     @JsonProperty("goin_to") String goingTo,
-    String date
+    LocalDateTime date
   ) {}
 
   public record SubscriberDto(String name, String email) {}
