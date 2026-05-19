571b69bcb0548c1e9ec6b0ec35856e18594a3b34
ferreira-gn <geanoficial14@gmail.com>
2026-05-19 10:19:38 -0300

    feat: create consumer to user deleted event

--- src/main/java/com/rota/facil/notification_service/messaging/consumers/RabbitAuthEventConsumer.java
+++ src/main/java/com/rota/facil/notification_service/messaging/consumers/RabbitAuthEventConsumer.java
@@ -41,4 +41,34 @@ public class RabbitAuthEventConsumer {
       "Finalizado processamento do envio de email do evento de usuário criado."
     );
   }
+
+  @RabbitListener(queues = "${rabbitmq.notification.user.deleted.queue}")
+  public void handleUserDeleted(AuthUserEventReceive authUserEventReceive) {
+    String userEmail = authUserEventReceive.email();
+    String subject = "Sua conta foi removida - Rota Fácil";
+    String templatePath = "emails/deleted-account";
+
+    if (userEmail.isEmpty() || userEmail == null) {
+      log.warn("Evento de usuário deletado recebido sem um email do usuário");
+      return;
+    }
+
+    Map<String, Object> templateVariables = Map.of(
+      "email",
+      userEmail,
+      "supportUrl",
+      "https://github.com/Rota-facil"
+    );
+
+    log.info(
+      "Iniciando envio de email de usuário deletado. email={}",
+      userEmail
+    );
+
+    emailService.sendEmail(userEmail, subject, templatePath, templateVariables);
+
+    log.info(
+      "Finalizado processamento do envio de email do evento de usuário deletado."
+    );
+  }
 }
