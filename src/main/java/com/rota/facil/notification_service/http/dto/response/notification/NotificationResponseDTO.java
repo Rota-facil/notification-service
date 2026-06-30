589b302e851263fa80344a3bf85252cdd6fa6557
Thauã Gabriel <tagashi.dev@gmail.com>
2026-06-30 15:38:17 -0300

    feat: add controller to list notifications

--- src/main/java/com/rota/facil/notification_service/http/dto/response/notification/NotificationResponseDTO.java
+++ src/main/java/com/rota/facil/notification_service/http/dto/response/notification/NotificationResponseDTO.java
@@ -1,4 +1,23 @@
-package com.rota.facil.notification_service.http.dto.response;
+package com.rota.facil.notification_service.http.dto.response.notification;
 
-public record NotificationResponseDTO() {
+import com.rota.facil.notification_service.domain.enums.NotificationType;
+import com.rota.facil.notification_service.domain.enums.Priority;
+import com.rota.facil.notification_service.domain.enums.RecipientType;
+import com.rota.facil.notification_service.domain.enums.TargetType;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record NotificationResponseDTO(
+        UUID id,
+        UUID recipientId,
+        UUID targetId,
+        RecipientType recipientType,
+        NotificationType notificationType,
+        String title,
+        String message,
+        TargetType targetType,
+        Priority priority,
+        LocalDateTime createdAt 
+) {
 }
